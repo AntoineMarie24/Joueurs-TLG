e35a14081abd28617690a312c2a01d538e6bb611
AntoineMarie24 <m-antoine76910@hotmail.fr>
2025-10-21 10:41:55 +0200

    Delete test 4 directory

--- test 4/script.js
+++
@@ -1,6 +0,0 @@
-const cards = document.querySelectorAll('.card-inner');
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    card.style.transform = card.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
-  });
-});
